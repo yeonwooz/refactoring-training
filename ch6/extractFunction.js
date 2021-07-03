6c964156b4fa47292f4fe54626fadac0fcc110ee
newnorm <newnorm@olulo.io>
2021-07-03 21:19:41 +0900

    original code

--- ch6/extractFunction.js
+++ ch6/extractFunction.js
@@ -1,8 +1,23 @@
 function printOwing(invoice) {
-    printBanner()
-    let outStanding = calculateOutstanding()
+    let outStanding = 0
 
-    console.log(`name is${invoice.customer}`)
-    console.log(`dept is ${outStanding}`)
+    printBanner()
+    
+
+    // calcutate the dept of customer
+    for (const o of invoice.orders) {
+        outStanding += o.amount
+    }
+
+    const today = Clock.today   // Date.now()
+    invoice.dueDate =  new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
+
+    console.log(`name ${invoice.customer}`)
+    console.log(`amount of dept ${outStanding}`)
+    console.log(`due date ${invoice.dueDate.toLocalDateString()}`)
+
+    function printBanner() {
+        console.log('dept of customer')
+    }
 }
 
