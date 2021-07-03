2acd30ea2bca8958769f72b302467c20d1c3672c
newnorm <newnorm@olulo.io>
2021-07-03 21:33:33 +0900

    split scopes

--- ch6/extractFunction.js
+++ ch6/extractFunction.js
@@ -1,26 +1,30 @@
 function printOwing(invoice) {
-    let outStanding = 0
-
     printBanner()
+    let outStanding = calculateOutStanding(invoice)
+    recordDueDate(invoice)
+    printDetails(invoice, outStanding)
+}
 
+function calculateOutStanding(invoice) {
+    let outStanding = 0
     // calcutate the dept of customer
     for (const o of invoice.orders) {
         outStanding += o.amount
     }
-
-    const today = Clock.today   // Date.now()
-    invoice.dueDate =  new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
-
-    printDetails(invoice, outStanding)
-
-    function printDetails(invoice, outStanding) {
-        console.log(`name ${invoice.customer}`)
-        console.log(`amount of dept ${outStanding}`)
-        console.log(`due date ${invoice.dueDate.toLocalDateString()}`)
-    }
-
-    function printBanner() {
-        console.log('dept of customer')
-    }
+    return outStanding
 }
 
+function recordDueDate(invoice) {
+    const today = Clock.today // Date.now()
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
+}
+
+function printDetails(invoice, outStanding) {
+    console.log(`name ${invoice.customer}`)
+    console.log(`amount of dept ${outStanding}`)
+    console.log(`due date ${invoice.dueDate.toLocalDateString()}`)
+}
+
+function printBanner() {
+    console.log('dept of customer')
+}
